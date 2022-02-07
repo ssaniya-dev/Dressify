9eb050aa178a41c6754931101b1a33ca96770888
alindquist540 <alindquist540@gmail.com>
2022-02-07 10:49:16 -0500

    hi

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loginscreen);
+        setContentView(R.layout.activity_logintest);
     }
     /**Called when the user taps the Send button */
     public void sendMessage(View view){

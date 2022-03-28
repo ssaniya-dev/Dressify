e417725437a568f5ac6fded127712126f264df30
Saniya S <62032120+ssaniya-dev@users.noreply.github.com>
2022-03-28 10:54:42 -0400

    added connections

--- app/src/main/java/com/example/loadingactivity/LoginScreen.java
+++ app/src/main/java/com/example/loadingactivity/LoginScreen.java
@@ -1,14 +1,28 @@
 package com.example.loadingactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class LoginScreen extends AppCompatActivity {
+public class LoginScreen extends AppCompatActivity implements View.OnClickListener{
+
+    public Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        login = (Button) findViewById(R.id.loginb);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(this, home.class);
+        startActivity(i);
     }
 }
